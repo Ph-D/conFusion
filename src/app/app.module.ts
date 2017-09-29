d4fae943c2432a58bcc5de085f717b30aec232ba
Philippe Doreau <p.doreau@criteo.com>
2017-09-29 17:04:31 +0200

    Application dish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MdToolbarModule } from '@angular/material';
+import {MatListModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatGridListModule} from '@angular/material';
+
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdToolbarModule,
+    MatListModule,
+    MatGridListModule,
     FlexLayoutModule
   ],
   providers: [],
