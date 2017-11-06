ac1af5f4ef198fb0a4f1edb70af2d15139d42fe0
Philippe Doreau <p.doreau@criteo.com>
2017-11-06 18:39:05 +0100

    Promise

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -2,20 +2,34 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
+
 @Injectable()
 export class DishService {
 
   constructor() { }
 
-  getDishes(): Dish[]{
-    return DISHES;
+  getDishes(): Promise<Dish[]> {
+    return new Promise(resolve => {
+      //Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES), 2000);
+    });
   }
 
-  getDish(id: number): Dish {
-      return DISHES.filter((dish) => (dish.id === id))[0];
-  }
+  getDish(id: number):Promise<Dish> {
+      return new Promise(resolve => {
+        //Simulate server latency with 2 second delay
+        setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+  });
+}
 
-  getFeaturedDish(): Dish {
-    return DISHES.filter((dish) => dish.featured)[0];
+  getFeaturedDish(): Promise<Dish> {
+    return new Promise(resolve => {
+      //Simulate server latency with 2 second delay
+      setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+    });
   }
 }
