18ff2904242ee9b4641946f670b1c099956a04d5
Philippe <phildoreau45@gmail.com>
2017-11-22 23:04:46 +0100

    Assignment Task1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -5,6 +5,7 @@ import { baseURL } from '../shared/baseurl';
 import { ProcessHTTPMsgService } from './process-httpmsg.service';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { RestangularModule, Restangular } from 'ngx-restangular';
 
 
 import 'rxjs/add/operator/delay';
@@ -13,22 +14,19 @@ import 'rxjs/add/observable/of';
 @Injectable()
 export class LeaderService {
 
-  constructor(private http: Http, private processHTTPMsgService : ProcessHTTPMsgService) { }
+  constructor(private restangular: Restangular,private http: Http, private processHTTPMsgService : ProcessHTTPMsgService) { }
 
   getLeaders(): Observable<Leader[]>{
-    return this.http.get(baseURL + 'leaders')
-    .map(res => { return this.processHTTPMsgService.extractData(res);
-    });
+    return this.restangular.all('leaders').getList();
   }
 
   getLeader(id: number): Observable<Leader> {
-    return this.http.get(baseURL + 'leaders/' + id)
-    .map(res => { return this.processHTTPMsgService.extractData(res)});
+    return this.restangular.one('leaders/', id).get();
   }
 
   getFeaturedLeader(): Observable<Leader> {
-    return this.http.get(baseURL + 'leaders?featured=true')
-    .map(res => { return this.processHTTPMsgService.extractData(res)[0]});
+    return this.restangular.all('leaders').getList({featured: true})
+    .map( leader => leader[0]);
   }
 
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -6,6 +6,7 @@ import { PROMOTIONS } from '../shared/promotions';
 import { Http, Response } from '@angular/http';
 import { baseURL } from '../shared/baseurl';
 import { Observable } from 'rxjs/Observable';
+import { RestangularModule, Restangular } from 'ngx-restangular';
 
 
 import 'rxjs/add/operator/delay';
@@ -16,26 +17,23 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class PromotionService {
 
-  constructor(private http: Http, private processHTTPMsgService : ProcessHTTPMsgService) { }
+  constructor(private restangular: Restangular,private http: Http, private processHTTPMsgService : ProcessHTTPMsgService) { }
 
 
   getPromotions(): Observable<Promotion[]>{
-    return this.http.get(baseURL + 'promotions')
-    .map(res => { return this.processHTTPMsgService.extractData(res);
-    });
+    return this.restangular.all('promotions').getList();
   }
 
   
   getPromotion(id: number): Observable<Promotion> {
-    return this.http.get(baseURL + 'promotions/' + id)
-    .map(res => { return this.processHTTPMsgService.extractData(res);
-    });
+    return this.restangular.one('promotions/', id).get();
   }
 
   getFeaturedPromotion(): Observable<Promotion> {
-    return this.http.get(baseURL + 'promotions?featured=true')
-    .map(res => { return this.processHTTPMsgService.extractData(res)[0]});
+    return this.restangular.all('promotions').getList({featured: true})
+    .map(promotion => promotion[0]);
   }
+
 
 
 }
